39f6947bd347e95201b464d7b7285e0664e801d6
user <user@LAPTOP-JNE495JN>
2024-07-25 20:53:01 +0300

    sil

---
+++ src/java101odevler/DörtVeBesinKatlari.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DörtVeBesinKatlari {
+   @SuppressWarnings("empty-statement")
+   public static void main(String[] args){
+       
+    int sayi,i,j;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Bir sayi giriniz:");
+    sayi =input.nextInt();
+    
+    for(i=1;i<sayi;i*=4){
+       System.out.println(i);
+   }       
+     System.out.println("-----------");  
+       
+    for(j=1;j<sayi;j*=5){
+       System.out.println(j);
+   }    
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+   } 
+}
