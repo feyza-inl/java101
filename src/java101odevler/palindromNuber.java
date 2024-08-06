62a567db761b0707643835e8f7f483978242f9e4
user <user@LAPTOP-JNE495JN>
2024-08-06 14:20:26 +0300

    Palindrom Sayi Kontrolü yapildi.

---
+++ src/java101odevler/palindromNuber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class palindromNuber {
+    
+    static void palindromNumber(int sayi){
+
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args){
+        
+        int sayi;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz:");
+        sayi = input.nextInt();
+        
+        palindromNumber(sayi);    
+    }
+}
